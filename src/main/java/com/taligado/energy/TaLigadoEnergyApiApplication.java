968193dcd33a99b441a0c3e0d112486e989a6a04
laiscrz <lais.albino2003@gmail.com>
2024-11-15 17:20:47 -0300

    feat: added spring boot project

---
+++ src/main/java/com/taligado/energy/TaLigadoEnergyApiApplication.java
@@ -0,0 +1,13 @@
+package com.taligado.energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaLigadoEnergyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaLigadoEnergyApiApplication.class, args);
+	}
+
+}
